d4e546edaa9d5cd03d170f8b6112e8810be67585
projecthanif <iamustpha213@gmail.com>
2023-08-23 15:05:51 +0100

    don

--- validation/user_verify.php
+++ validation/user_verify.php
@@ -1,20 +1,29 @@
 <?php
 require_once('../connection/connect.php');
 session_start();
+
 $name = $_POST['name'] ?? '';
 $password = $_POST['password'] ?? '';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
     $sql = "SELECT * FROM users
     WHERE user_email='$name';";
+
     $result = $conn->query($sql);
     $out = $result->fetch_assoc();
+
     if ($out == true) {
+
         $hash = $out['user_password'];
         $verify = password_verify($password, $hash);
+
         if ($verify) {
+
             $_SESSION['lname'] = $out['user_lname'];
             $_SESSION['type'] = $out['type'];
             $_SESSION['name'] = $out['user_fname'];
+
             header('Location: ../index.php');
             exit;
         } else {
@@ -26,6 +35,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 } else {
     header('Location: user_login.php');
-
 }
-?>
