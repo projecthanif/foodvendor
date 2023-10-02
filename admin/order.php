18169b8da66fc6c6e0bc01710a37d353d2ce6e7a
projecthanif <iamustpha213@gmail.com>
2023-10-02 12:48:16 +0100

    working on the UI for dashboard

--- admin/order.php
+++ admin/order.php
@@ -13,13 +13,14 @@ $query = $conn->query('SELECT * FROM user_order');
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="../assets/css/dStyle.css" />
-  <link rel="shortcut icon" href="../assets/img/bowl.png" type="image/x-icon">
-  <title>Order Page</title>
+  <link rel="stylesheet" href="../assets/css/style.css" />
+  <link rel="stylesheet" href="../assets/css/dStyle.css">
+  <link rel="shortcut icon" href="../assets/img/bowl.png" type="image/x-icon" />
+  <title>Food Vendor</title>
 </head>
 
 <body>
-  <nav id="nav">
+  <nav id="nav" class="navbar-large">
     <a href="../index.php">
       <img src="../assets/img/bowl.png" alt="" class="nav-brand" />
     </a>
@@ -30,7 +31,7 @@ $query = $conn->query('SELECT * FROM user_order');
       <li class="nav-item">
         <form action="#" class="form">
           <label for="search">
-            <input type="text" class="form-input" placeholder="Search" />
+            <input type="text" class="form-input" placeholder="Search" name="search" />
             <button class="btn-form">
               <i class="fa fa-search"></i>
             </button>
@@ -43,62 +44,54 @@ $query = $conn->query('SELECT * FROM user_order');
     </ul>
     <ul class="navbar list">
       <li class="nav-item">
-        <a href="#" class="nav-link">
-          <i class="fa fa-bell fa-2x"></i>
+        <a href="user.php" class="nav-link">
+          <i class="fa-regular fa-user fa-2x"></i>
         </a>
       </li>
       <li class="nav-item">
         <a href="#" class="nav-link">
-          <i class="fa fa-user fa-2x"></i>
-          <h6 class="username">John Doe</h6>
+          <i class="fa-regular fa-heart fa-2x"></i>
         </a>
       </li>
-      <!-- <li class="nav-item">
-            <a href="#" class="nav-link">
-            </a>
-          </li> -->
+      <li class="nav-item">
+        <a href="cart.php" class="nav-link">
+          <i class="fa fa-bag-shopping fa-2x"></i>
+        </a>
+      </li>
     </ul>
   </nav>
-
   <main>
-    <aside class="aside">
-      <ul class="asidelist">
-        <li class="asideItem">
-          <a href="#"><i class="fa fa-user"></i> Profile</a>
-        </li>
-        <li class="asideItem">
-          <a href="user.php"><i class="fa fa-user"></i> Users</a>
-        </li>
-        <li class="asideItem">
-          <a href="order.php">Orders</a>
-        </li>
-        <li class="asideItem">
-          <a href="#">Store</a>
-        </li>
-      </ul>
-    </aside>
     <section class="body">
-      <table class="table">
-        <thead>
-          <th>Customer Id</th>
-          <th>Product Id</th>
-          <th>Quantity</th>
-        </thead>
-        <?php
-        if (mysqli_num_rows($query) > 0) :
-          while ($orderInfo = $query->fetch_assoc()) :
-        ?>
-            <tbody>
-              <td><?= $orderInfo['userid'] ?></td>
-              <td><?= $orderInfo['productid'] ?></td>
-              <!-- <td><?= $orderInfo['timeofpurchae'] ?></td> -->
-              <td><?= $orderInfo['quantity'] ?></td>
-            </tbody>
-        <?php
-          endwhile;
-        endif;
-        ?>
-      </table>
+      <aside class="aside">
+        <a href="dashboard.html" class="nav-link" active><i class="fa fa-user"></i> Profile</a>
+        <a href="user.php" class="nav-link"><i class="fa fa-users"></i> Users</a>
+        <a href="order.php" class="nav-link">Orders</a>
+        <a href="store.php" class="nav-link">Store</a>
+      </aside>
+      <section class="section body">
+        <table class="table">
+          <thead>
+            <th>Customer Id</th>
+            <th>Product Id</th>
+            <!-- <th>Time of Purchase</th> -->
+            <th>Quantity</th>
+          </thead>
+          <?php
+          if (mysqli_num_rows($query) > 0) :
+            while ($orderInfo = $query->fetch_assoc()) :
+          ?>
+              <tbody>
+                <td><?= $orderInfo['userid'] ?></td>
+                <td><?= $orderInfo['productid'] ?></td>
+                <!-- <td><?= $orderInfo['timeofpurchase'] ?></td> -->
+                <td><?= $orderInfo['quantity'] ?></td>
+              </tbody>
+          <?php
+            endwhile;
+          endif;
+          ?>
+        </table>
+      </section>
     </section>
   </main>
 
